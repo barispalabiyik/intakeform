6c19976d4a47ede72df716e2f10b9ced613c0ad8
Baris Palabiyik <baris@ticketnetwork.com>
2023-01-03 19:28:58 +0300

    Conditions and all fields finished, Adress revisit

--- script.js
+++ script.js
@@ -291,64 +291,8 @@ $(document).ready(function () {
 
   // Guarantor Information Conditional Fields
 
-  const states = [
-    { value: "", name: "Select State" },
-    { value: "AK", name: "Alaska" },
-    { value: "TX", name: "Texas" },
-    { value: "AL", name: "Alabama" },
-    { value: "AR", name: "Arkansas" },
-    { value: "AZ", name: "Arizona" },
-    { value: "CA", name: "California" },
-    { value: "CO", name: "Colorado" },
-    { value: "CT", name: "Connecticut" },
-    { value: "DC", name: "DistrictofColumbia" },
-    { value: "DE", name: "Delaware" },
-    { value: "FL", name: "Florida" },
-    { value: "GA", name: "Georgia" },
-    { value: "HI", name: "Hawaii" },
-    { value: "IA", name: "Iowa" },
-    { value: "ID", name: "Idaho" },
-    { value: "IL", name: "Illinois" },
-    { value: "IN", name: "Indiana" },
-    { value: "KS", name: "Kansas" },
-    { value: "KY", name: "Kentucky" },
-    { value: "LA", name: "Louisiana" },
-    { value: "MA", name: "Massachusetts" },
-    { value: "MD", name: "Maryland" },
-    { value: "ME", name: "Maine" },
-    { value: "MI", name: "Michigan" },
-    { value: "MN", name: "Minnesota" },
-    { value: "MO", name: "Missouri" },
-    { value: "MS", name: "Mississippi" },
-    { value: "MT", name: "Montana" },
-    { value: "NC", name: "NorthCarolina" },
-    { value: "ND", name: "NorthDakota" },
-    { value: "NE", name: "Nebraska" },
-    { value: "NH", name: "NewHampshire" },
-    { value: "NJ", name: "NewJersey" },
-    { value: "NM", name: "NewMexico" },
-    { value: "NV", name: "Nevada" },
-    { value: "NY", name: "NewYork" },
-    { value: "OH", name: "Ohio" },
-    { value: "OK", name: "Oklahoma" },
-    { value: "OR", name: "Oregon" },
-    { value: "PA", name: "Pennsylvania" },
-    { value: "RI", name: "RhodeIsland" },
-    { value: "SC", name: "SouthCarolina" },
-    { value: "SD", name: "SouthDakota" },
-    { value: "TN", name: "Tennessee" },
-    { value: "TX", name: "Texas" },
-    { value: "UT", name: "Utah" },
-    { value: "VA", name: "Virginia" },
-    { value: "VT", name: "Vermont" },
-    { value: "WA", name: "Washington" },
-    { value: "WI", name: "Wisconsin" },
-    { value: "WV", name: "WestVirginia" },
-    { value: "WY", name: "Wyoming" },
-  ];
-
   states.forEach((state) => {
-    $("#guarantorState").append(
+    $("#guarantorState,#propertyState").append(
       $("<option></option>").attr("value", state.value).text(state.name)
     );
   });
@@ -375,18 +319,37 @@ $(document).ready(function () {
   // Conditional Field Logic End
 });
 
+// Validation
+
+jQuery.validator.addMethod(
+  "lettersonly",
+  function (value, element) {
+    return this.optional(element) || /^[a-z]+$/i.test(value);
+  },
+  "Letters only please"
+);
+
 $("#lenderIntakeForm").validate({
   rules: {
     propertyType: {
       required: true,
     },
-    street: {
+    propertyAddress: {
       required: true,
     },
-    city: {
+    propertyCity: {
       required: true,
     },
-    state: {
+    propertyState: {
+      required: true,
+    },
+    propertyZip: {
+      required: true,
+      number: true,
+      minLength: 5,
+      maxLength: 5,
+    },
+    describePType: {
       required: true,
     },
     loanProgram: {
@@ -399,10 +362,7 @@ $("#lenderIntakeForm").validate({
     loanType: {
       required: true,
     },
-    zip: {
-      required: true,
-      number: true,
-    },
+
     currentTotalIncome: {
       required: true,
       number: true,
@@ -411,7 +371,6 @@ $("#lenderIntakeForm").validate({
       required: true,
       number: true,
     },
-
     guarantorPhone: {
       required: true,
       number: true,
@@ -423,3 +382,59 @@ $("#lenderIntakeForm").validate({
     },
   },
 });
+
+const states = [
+  { value: "", name: "Select State" },
+  { value: "AK", name: "Alaska" },
+  { value: "TX", name: "Texas" },
+  { value: "AL", name: "Alabama" },
+  { value: "AR", name: "Arkansas" },
+  { value: "AZ", name: "Arizona" },
+  { value: "CA", name: "California" },
+  { value: "CO", name: "Colorado" },
+  { value: "CT", name: "Connecticut" },
+  { value: "DC", name: "DistrictofColumbia" },
+  { value: "DE", name: "Delaware" },
+  { value: "FL", name: "Florida" },
+  { value: "GA", name: "Georgia" },
+  { value: "HI", name: "Hawaii" },
+  { value: "IA", name: "Iowa" },
+  { value: "ID", name: "Idaho" },
+  { value: "IL", name: "Illinois" },
+  { value: "IN", name: "Indiana" },
+  { value: "KS", name: "Kansas" },
+  { value: "KY", name: "Kentucky" },
+  { value: "LA", name: "Louisiana" },
+  { value: "MA", name: "Massachusetts" },
+  { value: "MD", name: "Maryland" },
+  { value: "ME", name: "Maine" },
+  { value: "MI", name: "Michigan" },
+  { value: "MN", name: "Minnesota" },
+  { value: "MO", name: "Missouri" },
+  { value: "MS", name: "Mississippi" },
+  { value: "MT", name: "Montana" },
+  { value: "NC", name: "NorthCarolina" },
+  { value: "ND", name: "NorthDakota" },
+  { value: "NE", name: "Nebraska" },
+  { value: "NH", name: "NewHampshire" },
+  { value: "NJ", name: "NewJersey" },
+  { value: "NM", name: "NewMexico" },
+  { value: "NV", name: "Nevada" },
+  { value: "NY", name: "NewYork" },
+  { value: "OH", name: "Ohio" },
+  { value: "OK", name: "Oklahoma" },
+  { value: "OR", name: "Oregon" },
+  { value: "PA", name: "Pennsylvania" },
+  { value: "RI", name: "RhodeIsland" },
+  { value: "SC", name: "SouthCarolina" },
+  { value: "SD", name: "SouthDakota" },
+  { value: "TN", name: "Tennessee" },
+  { value: "TX", name: "Texas" },
+  { value: "UT", name: "Utah" },
+  { value: "VA", name: "Virginia" },
+  { value: "VT", name: "Vermont" },
+  { value: "WA", name: "Washington" },
+  { value: "WI", name: "Wisconsin" },
+  { value: "WV", name: "WestVirginia" },
+  { value: "WY", name: "Wyoming" },
+];
